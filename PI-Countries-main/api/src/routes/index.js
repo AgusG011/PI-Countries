8f6ed565594440b94243a2e75dfe0ffdcacb3c23
Agustín Guerrero <agustinguerrero642@gmail.com>
2022-12-22 14:33:01 -0300

    backend

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -1,12 +1,9 @@
-// const { default: axios } = require('axios');
 const { Router } = require('express');
 const { countryRoute } = require('./country');
 const activityRoute = require('./activity');
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
 
 router.use('/countries', countryRoute)
