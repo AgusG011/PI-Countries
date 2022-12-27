c1afae0d5a6a28f744012a73e69f9f5b6f8cc201
Agustín Guerrero <agustinguerrero642@gmail.com>
2022-12-27 17:32:16 -0300

    .

---
+++ PI-Countries-main/client/src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export  function getcountry (){        //se realiza conexion entre back y frond
+
+   return async function(dispatch){
+
+    var json = await axios.get('http://localhost:3001/Country');
+  
+    return dispatch({
+        type:'GET_COUNTRY',
+        payload:json.data,         
+    })
+   }
+}
