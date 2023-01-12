26341b9a13c813d1476062e22168bd04cec41e94
Agustín Guerrero <agustinguerrero642@gmail.com>
2023-01-12 17:20:38 -0300

    .

--- PI-Countries-main/client/src/components/Cards/Cards.jsx
+++ PI-Countries-main/client/src/components/Cards/Cards.jsx
@@ -2,29 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../Cards/Cards.css';
 
+
     export default function Card({ name, flag, continent, id}){
         return(
-            <section className="cards">
-                <div className="flip-card">
-                <div className="flip-card-inner">
-                    <div className="flip-card-front">                       
-                        <Link to={'/home' + id}>
-                    <div className="flip-card-back">
+                <section className="cards">
+            <div className="flip-card">
+                <div>
+                    <img className="img" src={flag} alt="No se encontro la bandera"/>
+                </div>
+                <Link to={'/home' + id}>
+                    <div>
                         <h3 className="link">{name}</h3>
                     </div>
                 </Link>
-                <div>
-                    <img className="img-c" src={flag} alt="No se encontro la bandera"/>
-                </div>
+                
                 <div className="continent">
                     <h3>{continent}</h3>
                 </div>
-            </div>    
-            </div>
-                </div>
-                   
-                    
+            </div>        
             </section>
-             
-    )}
+        )
+    }
         
--- PI-Countries-main/client/src/components/LandingPage/LandingPage.jsx
+++ PI-Countries-main/client/src/components/LandingPage/LandingPage.jsx
@@ -7,7 +7,7 @@ export default function LandingPage(){
         <div className='landing'>
           
             <h1 > Bien venido! </h1> 
-            <img src={'https://i.pinimg.com/originals/5f/0f/6c/5f0f6c850f25343e35b5e46f4bd51cfa.jpg'} alt="" className='imagen' />
+            <img src={'https://i.pinimg.com/originals/1b/61/c5/1b61c518124ddc4ef9da739c6b5cc1bd.png'} alt="" className='imagen' />
             <Link to ='/home'><button className='btn'> Ver mas </button></Link>
             
         </div>
