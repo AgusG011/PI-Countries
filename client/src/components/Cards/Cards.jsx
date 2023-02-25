1432c36a22a527059969fc75ee4aefcf00b970c3
Agustín Guerrero <agustinguerrero642@gmail.com>
2023-02-25 14:32:00 -0300

    detallitos

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -5,19 +5,20 @@ import '../Cards/Cards.css';
 
     export default function Card({ name, flag, continent, id}){
         return(
-                <section className="cards">
-            <div className="flip-card">
-                <div>
-                    <img className="img" src={flag} alt="No se encontro la bandera"/>
-                </div>
-                
-                <h3 className= "link">{name}</h3>
-                
-                <div className="continent">
-                    <h3>{continent}</h3>
-                </div>
-            </div>        
+            <section className="card">
+                <div className="flip-card">
+                    
+                        <img className="img" src={flag} alt="No se encontro la bandera"/>
+                   
+                    
+                    <h3 className= "link">{name}</h3>
+                    
+                    <div className="continent">
+                        <h3>{continent}</h3>
+                    </div>
+                </div>        
             </section>
         )
     }
+    
         
