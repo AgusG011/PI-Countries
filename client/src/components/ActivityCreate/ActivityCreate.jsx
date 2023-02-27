5e3db7dba468388ea9c059059f5c6883ec126f4d
Agustín Guerrero <agustinguerrero642@gmail.com>
2023-02-27 12:43:24 -0300

    .

--- client/src/components/ActivityCreate/ActivityCreate.jsx
+++ client/src/components/ActivityCreate/ActivityCreate.jsx
@@ -105,7 +105,15 @@ export default function ActivityCreate(){
 
     return(
         <div className="padre">
-            <div class="card">
+            <div class="card-create">
+                    <div className="back">
+                        <Link to="/home">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z"/>
+                            </svg>
+                        </Link>
+                    </div>
+
                 <span class="card__title">Create your activitie</span>
                     <div class="card__form">
                     <div>
@@ -179,7 +187,7 @@ export default function ActivityCreate(){
                         )}
                             </div>
                             <br/><br/>
-                        <button class="sign-up"type="submit" >Create</button>
+                        <button onClick={handleSubmit} class="sign-up"type="submit" >Create</button>
                     </div>
                 </div>
             </div>
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -54,9 +54,13 @@ export default function Detail(props){
                         <h2>Poblacion: {data[0].population}</h2>
                         </div>
 
-                        <Link to= '/home'>
-                            <button className="volver">Volver</button>
-                        </Link>
+                        <div className="back-detail">
+                            <Link to="/home">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z"/>
+                                </svg>
+                            </Link>
+                        </div>
                     </div>
                     
                     <div >
@@ -70,7 +74,16 @@ export default function Detail(props){
                                 <h2>Duracion: {e.duration}</h2>
                                 <h2>Temporada: {e.season}</h2>
 
-                                <button onClick = {()=>ActivtyDelete(e.id)} className="elm">Eliminar</button>
+
+
+                                <div className="elm">
+                                    <button onClick = {()=>ActivtyDelete(e.id)} class="noselect"><span class="text">Delete</span><span class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                            <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                                        </svg></span>
+                                    </button>
+                                
+                                </div>
 
                             </li>
                            )}
